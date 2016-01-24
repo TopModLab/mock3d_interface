a909679e0bb9778e7ad19463c4c69a81d19c257c
Yinan Xiong <yvonnexiong1114@gmail.com>
2016-01-23 23:40:56 -0600

    Set different ratio Image
    Done! Edit in:
    dragndrop.js, css, index

--- dragndrop.js
+++ dragndrop.js
@@ -1,5 +1,7 @@
 var image1, image2, image3, image4, image5;
+
 var UPLOAD = UPLOAD || {};
+
 UPLOAD.main = new function()
 {
 	// init vars
@@ -106,9 +108,15 @@ UPLOAD.main = new function()
 			{
 				image1 		= document.createElement('img');
 				image1.src 	= event.target.result;
-				image1.style.width = "100px";
 				image = image1;
 
+				//set thumb image size
+				if(image1.width>=image1.height){
+					image1.style.width = "100px";
+				}else{
+					image1.style.height = "100px";
+				}
+				
 				// Update WebGL texture.
 				darkImage.src = image1.src;
 			}
@@ -116,8 +124,14 @@ UPLOAD.main = new function()
 			{
 				image2 		= document.createElement('img');
 				image2.src 	= event.target.result;
-				image2.style.width = "100px";
 				image = image2;
+				
+				//set thumb image size
+				if(image2.width>=image2.height){
+					image2.style.width = "100px";
+				}else{
+					image2.style.height = "100px";
+				}
 
 				// Update WebGL texture.
 				lightImage.src = image2.src;
@@ -126,18 +140,53 @@ UPLOAD.main = new function()
 			{
 				image3 		= document.createElement('img');
 				image3.src 	= event.target.result;
-				image3.style.width = "100px";
 				image = image3;
 
+				//set thumb image size
+				if(image3.width>=image3.height){
+					image3.style.width = "100px";
+				}else{
+					image3.style.height = "100px";
+				}
+				//get thumbWidth and thumbHeight & update gl-canvas width and height
+				var canvas = document.getElementById("gl-canvas");
+				var canvasContainer = $('.canvas_container');
+				var ratioImage3 = image3.width / image3.height;
+				var ratioContainer = canvasContainer.width() / canvasContainer.height();
+				
+				if(ratioImage3>=ratioContainer){
+					canvas.width = canvasContainer.width();
+					canvas.height = canvas.width * image3.height / image3.width;
+					canvas.style.width = "100%";
+					canvas.style.height = "auto";
+				}else{
+					canvas.height = canvasContainer.height() ;
+					canvas.width = canvas.height * image3.width / image3.height;
+					canvas.style.height = "100%";
+					canvas.style.width = "auto";
+				}
+				
+				
+				gl = WebGLUtils.setupWebGL( canvas );
+    			gl.viewport( 0, 0, canvas.width, canvas.height );
+    
+    
 				// Update WebGL texture.
 				normalImage.src = image3.src;
+
 			}
 			else if(elemName === "container4")
 			{
 				image4 		= document.createElement('img');
 				image4.src 	= event.target.result;
-				image4.style.width = "100px";
 				image = image4;
+
+				//set thumb image size
+				if(image4.width>=image4.height){
+					image4.style.width = "100px";
+				}else{
+					image4.style.height = "100px";
+				}
 
 				// Update WebGL texture.
 				reflectImage.src = image4.src;
@@ -146,8 +195,14 @@ UPLOAD.main = new function()
 			{
 				image5 		= document.createElement('img');
 				image5.src 	= event.target.result;
-				image5.style.width = "100px";
 				image = image5;
+
+				//set thumb image size
+				if(image5.width>=image5.height){
+					image5.style.width = "100px";
+				}else{
+					image5.style.height = "100px";
+				}
 
 				// Update WebGL texture.
 				refractImage.src = image5.src;
